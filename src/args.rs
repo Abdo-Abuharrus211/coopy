a581261d5f462dadc90a9065c5d17b6cfcf2a962
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-09-02 15:13:28 -0700

    Implement function to process args

--- src/args.rs
+++ src/args.rs
@@ -33,7 +33,53 @@ pub struct Args {
 
 impl Args {
     /// Process the 'add' and 'del' commands and their potential args 'folders' and 'forbidden'
-    pub fn process_args(&self) {}
+    pub fn process_args(&self) {
+        if let Some(arg_s) = &self.source {
+            println!("Syncing from: {}", arg_s);
+            // TODO: placeholder for saving and overwriting the config!
+        }
+        if let Some(arg_t) = &self.target {
+            println!("Copying to: {}", arg_t);
+            // ...
+        }
+
+        match &self.command {
+            Some(Commands::Add { kind, value }) => {
+                let folder_names: Vec<String> = Self::split_vals(&value);
+                // State::update_config("add", &kind, &folder_names);
+                // let _ = folder_names.into_iter().map(|f_name| {
+                //     println!("Adding folder: {}", f_name);
+                //     State::update_config("add", &folder_names);
+                // });
+                // ...
+                match kind.as_str() {
+                    "folders" => {
+                        // TODO: push to the folders array in config.toml
+                        let conf_file = fs::read_to_string(CONFIG_FILE).unwrap();
+                        
+                    }
+                    "forbidden" => {
+                        //...
+                    }
+                    _ => {
+                        eprintln!("BOB!") //TODO: change this, pls...
+                    }
+                }
+            }
+            Some(Commands::Delete { kind, value }) => {
+                //...
+            }
+            _ => {
+                eprintln!("Unknown command!");
+            }
+        }
+    }
+
+    fn split_vals(vals: &String) -> Vec<String> {
+        let values: Vec<String> = vals.split(',').map(String::from).collect();
+        values
+    }
+
     // TODO: implement these functions to update the config file
     // fn add_folders() {}
     //
