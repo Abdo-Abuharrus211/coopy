fd9ec5663f60f645cf9a6e3ff5e0f50ae4e0b551
abdu <aabuharrus@gmail.com>
2026-02-06 13:15:06 -0800

    Move main fn to top of module

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,50 @@ pub const CONFIG_FILE: &str = "config.toml";
 //     forbidden: Vec<String>,
 // }
 
+
+fn main() -> Result<(), io::Error> {
+    // Processing command line args, if any
+    let command_args: args::Args = read_args();
+    if command_args.source.is_some()
+        || command_args.target.is_some()
+        || command_args.command.is_some()
+    {
+        command_args.process_args();
+    }
+
+    let conf_contents = match fs::read_to_string(CONFIG_FILE) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Error reading config file: {}", CONFIG_FILE);
+            exit(1);
+        }
+    };
+    // The data's serialized as a Config Struct incl. the UserConf struct for user settings.
+    let settings: Config = match toml::from_str(&conf_contents) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Error parsing settings from: {}", e);
+            exit(1);
+        }
+    };
+
+    let mut current_state = State { config: settings };
+    current_state.load_paths(command_args.source, command_args.target);
+
+    let formatted_source = current_state.config.source.trim().to_string();
+    let formatted_target = current_state.config.target.trim().to_string();
+    let targeted_files = current_state.traverse_folder(Path::new(&formatted_source), "")?;
+    println!("Copying {} files...", targeted_files.len());
+    let success = sync_files(&targeted_files, &formatted_source, &formatted_target);
+    if success {
+        println!("Sync completed Successfully!");
+    } else {
+        println!("Sync completed with some failures");
+    }
+    Ok(())
+}
+
+
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all(serialize = "kebab-case", deserialize = "kebab-case"))]
 struct Config {
@@ -76,7 +120,7 @@ impl State {
         Ok(tar_files)
     }
 
-    fn prompt_user_paths(&mut self) {&
+    fn prompt_user_paths(&mut self) {
         println!("Obsidian vault's (source) path.");
         io::stdin()
             .read_line(&mut self.config.source)
@@ -101,47 +145,6 @@ impl State {
     }
 }
 
-fn main() -> Result<(), io::Error> {
-    // Processing command line args, if any
-    let command_args: args::Args = read_args();
-    if command_args.source.is_some()
-        || command_args.target.is_some()
-        || command_args.command.is_some()
-    {
-        command_args.process_args();
-    }
-
-    let conf_contents = match fs::read_to_string(CONFIG_FILE) {
-        Ok(c) => c,
-        Err(_) => {
-            eprintln!("Error reading config file: {}", CONFIG_FILE);
-            exit(1);
-        }
-    };
-    // The data's serialized as a Config Struct incl. the UserConf struct for user settings.
-    let settings: Config = match toml::from_str(&conf_contents) {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("Error parsing settings from: {}", e);
-            exit(1);
-        }
-    };
-
-    let mut current_state = State { config: settings };
-    current_state.load_paths(command_args.source, command_args.target);
-
-    let formatted_source = current_state.config.source.trim().to_string();
-    let formatted_target = current_state.config.target.trim().to_string();
-    let targeted_files = current_state.traverse_folder(Path::new(&formatted_source), "")?;
-    println!("Copying {} files...", targeted_files.len());
-    let success = sync_files(&targeted_files, &formatted_source, &formatted_target);
-    if success {
-        println!("Sync completed Successfully!");
-    } else {
-        println!("Sync completed with some failures");
-    }
-    Ok(())
-}
 
 fn sync_files(files: &Vec<String>, src: &String, tgt: &String) -> bool {
     let mut success = true;
