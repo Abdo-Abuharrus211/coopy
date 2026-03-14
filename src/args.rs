aaf574004916c62b76b418d2cc9008e74f2b3785
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-03-14 01:47:44 -0700

    Major refactor of `Args::process/1` and modify fields' names and types for `Args` and `Commands` structs

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,10 @@
 use crate::{CONFIG_FILE, State};
-use clap::builder::Str;
-use clap::{Command, Parser, Subcommand};
-use std::fs;
-use toml::Value;
+use clap::{Parser, Subcommand};
 
-pub enum ConfigArraysFields {
+#[derive(clap::ValueEnum, Debug, Clone)]
+pub enum ConfigFields {
+    Source,
+    Target,
     Folders,
     Forbidden,
 }
@@ -13,17 +13,17 @@ pub enum ConfigArraysFields {
 pub enum Commands {
     /// Add items to a config array
     Add {
-        kind: ConfigArraysFields,
+        kind: ConfigFields,
         /// Vector of strings
         values: Vec<String>,
     },
     /// Remove items from a config array
     Rmv {
-        kind: ConfigArraysFields,
+        kind: ConfigFields,
         values: Vec<String>,
     },
     Set {
-        kind: ConfigArraysFields,
+        kind: ConfigFields,
         path: String,
     },
     /// Shows the current config
@@ -36,75 +36,50 @@ pub struct Args {
     /// The first positional argument it the path of vault to copy from
     #[arg()]
     pub source: Option<String>,
-    /// Path to copy to
+    /// Second positional argument, path to copy to
     #[arg()]
     pub target: Option<String>,
-
+    /// Potential subcommands
     #[command(subcommand)]
     pub subcommand: Option<Commands>,
 }
 
 impl Args {
     /// Process the 'add' and 'del' commands and their potential args 'folders' and 'forbidden'
-    pub fn process_args(&self) {
-        if let Some(arg_s) = &self.source {
-            println!("Syncing from: {}", arg_s);
-            // TODO: placeholder for saving and overwriting the config!
-        }
-        if let Some(arg_t) = &self.target {
-            println!("Copying to: {}", arg_t);
-            // ...
-        }
-
+    pub fn process(&self) -> Result<(), String> {
+        // TODO: anything with the `source` and `target`?
+        // if let Some(arg_s) = &self.source {
+        //     // do something
+        // }
+        // if let Some(arg_t) = &self.target {
+        //     println!("Copying to: {}", arg_t);
+        //     // ...
+        // }
         match &self.subcommand {
-            Some(Commands::Add {
-                kind,
-                values: value,
-            }) => {
-                let folder_names: Vec<String> = Self::split_vals(&value);
-                // State::update_config("add", &kind, &folder_names);
-                // let _ = folder_names.into_iter().map(|f_name| {
-                //     println!("Adding folder: {}", f_name);
-                //     State::update_config("add", &folder_names);
-                // });
-                // ...
-                match kind.as_str() {
-                    "folders" => {
-                        // TODO: push to the folders array in config.toml
-                        let conf_file = fs::read_to_string(CONFIG_FILE).unwrap();
-                    }
-                    "forbidden" => {
-                        //...
-                    }
-                    _ => {
-                        eprintln!("BOB!") //TODO: change this, pls...
-                    }
-                }
+            Some(Commands::Add { kind, values }) => {
+                State::update_config("add", Some(kind), Some(values), None);
             }
-            Some(Commands::Rmv { kind, name: value }) => {
-                //...
+            Some(Commands::Rmv { kind, values }) => {
+                State::update_config("rmv", Some(kind), Some(values), None);
+            }
+            Some(Commands::Set { kind, path }) => {
+                State::update_config("set", Some(kind), None, Some(&path));
+            }
+            Some(Commands::Config) => {
+                println!(
+                    "Place holder until I figure out what to print from {}",
+                    CONFIG_FILE
+                );
             }
             _ => {
-                eprintln!("Unknown command!");
+                return Err(String::from("Unknown command!"));
             }
         }
+        Ok(())
     }
 
     fn split_vals(vals: &String) -> Vec<String> {
         let values: Vec<String> = vals.split(',').map(String::from).collect();
         values
     }
-
-    // TODO: implement these functions to update the config file
-    // fn add_folders() {}
-    //
-    // fn del_folders() {}
-    //
-    // fn add_forbidden() {}
-    //
-    // fn del_forbidden() {}
-}
-
-pub fn read_args() -> Args {
-    Args::parse()
 }
