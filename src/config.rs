bbad43b9592d3ab8fdcb17a5086dc15b2900cf29
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-07-17 12:16:13 -0700

    Refactor all config related fn into this module - added user IO prompt

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
-use crate::args::ConfigFields;
-use crate::{CONFIG_FILE, util};
+use crate::cli::ConfigFields;
+use crate::{CONFIG_FILE};
 use serde::{Deserialize, Serialize};
-use std::path::Path;
 use std::process::exit;
 use std::{fs, io};
 use toml::Table;
@@ -9,8 +8,8 @@ use toml::Table;
 pub struct UserSettings {
     pub(crate) source: String,
     pub(crate) target: String,
-    folders: Vec<String>,
-    forbidden: Vec<String>,
+    pub folders: Vec<String>,
+    pub forbidden: Vec<String>,
 }
 
 // Struct Definitions
@@ -25,48 +24,6 @@ pub struct State {
 }
 
 impl State {
-    /// Traverse the given directory.
-    /// Recursively traverses the directory for files and checks if they're allowed/forbidden.
-    pub fn traverse_folder(&self, start: &Path, relative_path: &str) -> io::Result<Vec<String>> {
-        let mut tar_files: Vec<String> = Vec::new();
-        if start.is_dir() {
-            for entry in fs::read_dir(start)? {
-                // Don't delete in favor for daisy-chaining, need these
-                let path = entry?.path();
-                let entry_name = path.file_name().unwrap().to_string_lossy();
-                let new_rel_path =
-                    util::build_rel_path(Path::new(&entry_name.to_string()), relative_path);
-
-                if path.is_dir() {
-                    let entry_str = &entry_name;
-                    // Check if the directory is clear to proceed - includes parent folders...
-                    if self
-                        .config
-                        .user_settings
-                        .folders
-                        .iter()
-                        .any(|f| f == entry_str)
-                        || !self
-                            .config
-                            .user_settings
-                            .forbidden
-                            .iter()
-                            .any(|f| f == entry_str)
-                    {
-                        let sub_dirs = self.traverse_folder(&path, &new_rel_path)?;
-                        tar_files.extend(sub_dirs);
-                    }
-                } else if path.is_file() && util::check_file(&path) {
-                    println!("Adding file {}", new_rel_path);
-                    tar_files.push(new_rel_path);
-                }
-            }
-        } else if start.is_file() && util::check_file(&start) {
-            tar_files.push(util::build_rel_path(start, relative_path));
-        }
-        Ok(tar_files)
-    }
-
     pub fn update_config(
         &mut self,
         operation: &str,
@@ -159,7 +116,7 @@ impl State {
         }
         // Prompt User for paths if they're not saved in the config file
         else if current.source.is_empty() && current.target.is_empty() {
-            util::prompt_user_paths(self);
+            prompt_user_paths(self);
         }
     }
 }
@@ -181,4 +138,22 @@ fn remove_values(list: &mut Vec<String>, values: Option<&[String]>) {
             }
         }
     }
+}
+
+/// Ask the user to provide Obsidian vault and destination paths
+pub fn prompt_user_paths(state: &mut State) {
+    let mut src_input = String::new();
+    let mut target_input = String::new();
+
+    print!("Obsidian vault's (source) path:");
+    io::stdin()
+        .read_line(&mut src_input)
+        .expect("Error reading source path!");
+    print!("Target path: ");
+    io::stdin()
+        .read_line(&mut target_input)
+        .expect("Error reading target path!");
+
+    state.config.user_settings.source = src_input.trim().to_string();
+    state.config.user_settings.target = target_input.trim().to_string();
 }
